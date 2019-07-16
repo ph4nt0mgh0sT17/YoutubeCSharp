a14f94e10664e3c2e253ae9fe996cd2a26708eaa
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-16 14:31:31 +0200

    Add project files.

---
+++ YoutubeCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Type a second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int result = firstNumber + secondNumber;
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
